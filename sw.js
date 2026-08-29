9eabbab64fb9439c3c7a1899e88c3aa6ab25c366
comunidadedefe <remirotempooficial@gmail.com>
2026-08-29 17:19:20 -0300

    Atualizar cache para o visual V5

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "biblia-2027-v7";
+const CACHE = "biblia-2027-v8";
 const ASSETS = [
-  "./","./index.html","./styles.css","./enhancements.css","./app.js","./calendar-2027.js","./data.js","./manifest.webmanifest","./icon.svg",
+  "./","./index.html","./styles.css","./enhancements.css","./visual-v5.css","./app.js","./calendar-2027.js","./visual-v5.js","./data.js","./manifest.webmanifest","./icon.svg",
   "./banner-data/chunk01.js","./banner-data/chunk02.js","./banner-data/chunk03.js","./banner-data/chunk04.js",
   "./banner-data/chunk05.js","./banner-data/chunk06.js","./banner-data/chunk07.js","./banner-data/chunk08.js","./banner-data/init.js"
 ];
